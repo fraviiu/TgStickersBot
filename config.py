9167a6bfcc56aa19482b3888c667858cc6ebd17b
14iq <60758749+14iq@users.noreply.github.com>
2020-05-25 11:18:50 +0300

    config proxy

--- config.py
+++ config.py
@@ -11,4 +11,7 @@ logsfile = 'botlogs.log'
 # токен бота из переменной окруженья
 token = environ['TOKEN']
 # сыллка на прокси
-proxy_url = 'socks5://95.179.155.139:1080'
+try:
+	proxy_url = environ['PROXY']
+except KeyError:
+	proxy_url = None
